53bae1f9d868adb7e7a75d6639598333c7d4b8e5
Ajlan0012 <168438131+Ajlan0012@users.noreply.github.com>
2025-08-16 22:38:30 +0530

    Add files via upload

---
+++ ArrayMinMax.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ArrayMinMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many numbers: ");
+        int n = sc.nextInt();
+
+        if (n < 2) {
+            System.out.println("Need at least 2 numbers!");
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int smallest = arr[0];
+        int largest = arr[0];
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num < smallest) {
+                smallest = num;
+            }
+            if (num > largest) {
+                secondLargest = largest; // Update second largest
+                largest = num;
+            } else if (num > secondLargest && num < largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Smallest element: " + smallest);
+        System.out.println("Largest element: " + largest);
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Second largest does not exist (all numbers equal).");
+        } else {
+            System.out.println("Second largest element: " + secondLargest);
+        }
+
+        sc.close();
+    }
+}
