ba1fc9723347443c8cdb19c8ea39027c3ec72ba1
byte_orbit <168438131+Byte0rbit@users.noreply.github.com>
2025-08-19 15:18:24 +0530

    TriangleTypeAndArea.java

--- TriangleTypeAndArea.java
+++ TriangleTypeAndArea.java
@@ -14,27 +14,21 @@ public class TriangleTypeAndArea {
         double c = sc.nextDouble();
 
         if (a + b > c && a + c > b && b + c > a) {
-
             if (a == b && b == c) {
                 System.out.println("The triangle is Equilateral.");
-            } 
-            else if (a == b || b == c || a == c) {
+            } else if (a == b || b == c || a == c) {
                 System.out.println("The triangle is Isosceles.");
-            } 
-            else {
+            } else {
                 System.out.println("The triangle is Scalene.");
             }
 
-            double s = (a + b + c) / 2; 
+            double s = (a + b + c) / 2;
             double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-
             System.out.println("Area of the triangle: " + area);
-        } 
-        else {
+        } else {
             System.out.println("The given sides do not form a valid triangle.");
         }
 
         sc.close();
     }
 }
-
