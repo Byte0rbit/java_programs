18ddb77747dd8b4d46651d3519e7b5cd18a0a908
byte_orbit <168438131+Byte0rbit@users.noreply.github.com>
2025-08-23 23:03:40 +0530

    ArithmeticOp.java

--- ArithmeticOp.java
+++ ArithmeticOp.java
@@ -1,33 +1,28 @@
-import java.util.Scanner;
-
 public class ArithmeticOp {
-	public static void main(String args[]){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter first number: ");
-		int num1 = input.nextInt();
+    public static void main(String args[]) {
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers as command line arguments.");
+            return;
+        }
 
-		System.out.print("Enter second number: ");
-		int num2 = input.nextInt();
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
 
-		int sum = num1 + num2;
-		int difference = num1 - num2;
-		int product = num1 * num2;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
 
-		if(num2 != 0){
-			int quotient = num1/num2;
-			int reminder = num1 % num2;
-		
-			System.out.println("Sum = " + sum);
-			System.out.println("Difference = " + difference);
-			System.out.println("Product = " + product);
-			System.out.println("Quotient = " + quotient);
-			System.out.println("Reminder = " + reminder);
-		}else{
-			System.out.println("Sum: " + sum);
-           		System.out.println("Difference: " + difference);
-            		System.out.println("Product: " + product);
-            		System.out.println("Cannot divide by zero. Quotient and remainder are undefined.");
-        	}
-	input.close();
-	}
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+
+        if (num2 != 0) {
+            int quotient = num1 / num2;
+            int remainder = num1 % num2;
+            System.out.println("Quotient = " + quotient);
+            System.out.println("Remainder = " + remainder);
+        } else {
+            System.out.println("Cannot divide by zero. Quotient and remainder are undefined.");
+        }
+    }
 }
