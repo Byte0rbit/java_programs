85a8308a464771c53648bf63648ff5778f9b55e6
byte_orbit <168438131+Byte0rbit@users.noreply.github.com>
2025-08-24 22:59:03 +0530

    ReverseAndSum.java

--- ReverseAndSum.java
+++ ReverseAndSum.java
@@ -1,21 +1,46 @@
 import java.util.Scanner;
 
 public class ReverseAndSum {
-    public static void main(String[] args) {
-        System.out.println("Enter the number:  ");
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int sum = 0, reverse = 0;
+    int number;
+    int sum;
+    int reverse;
 
-        int temp = num;
+    public ReverseAndSum(int num) {
+        number = num;
+        sum = 0;
+        reverse = 0;
+    }
+
+    public void calculate() {
+        int temp = number;
         while (temp > 0) {
             int digit = temp % 10;
-            sum += digit;
+            sum = sum + digit;
             reverse = reverse * 10 + digit;
-            temp /= 10;
+            temp = temp / 10;
         }
+    }
 
-        System.out.println("Sum of digits: " + sum);
-        System.out.println("Reverse: " + reverse);
+    
+    public int getSum() {
+        return sum;
+    }
+
+    public int getReverse() {
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int input = sc.nextInt();
+
+        ReverseAndSum obj = new ReverseAndSum(input);
+        obj.calculate();
+
+        System.out.println("Sum of digits: " + obj.getSum());
+        System.out.println("Reverse: " + obj.getReverse());
+
+        sc.close();
     }
 }
