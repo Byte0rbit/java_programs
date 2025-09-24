3cdecfcaa4b2b48ba5069e9c00289de1810d0bac
byte_orbit <168438131+Byte0rbit@users.noreply.github.com>
2025-09-24 22:19:38 +0530

    BaseConversion.java

--- BaseConversion.java
+++ BaseConversion.java
@@ -1,22 +1,49 @@
 import java.util.Scanner;
 
 public class BaseConversion {
+
+    static String toBinary(int num) {
+        StringBuilder binary = new StringBuilder();
+        int n = num;
+        while (n > 0) {
+            int rem = n % 2;
+            binary.insert(0, rem);
+            n = n / 2;
+        }
+        return (binary.length() > 0) ? binary.toString() : "0";
+    }
+
+    static String toOctal(int num) {
+        StringBuilder octal = new StringBuilder();
+        int n = num;
+        while (n > 0) {
+            int rem = n % 8;
+            octal.insert(0, rem);
+            n = n / 8;
+        }
+        return (octal.length() > 0) ? octal.toString() : "0";
+    }
+
+    static String toHexadecimal(int num) {
+        StringBuilder hex = new StringBuilder();
+        int n = num;
+        char[] hexDigits = {'0','1','2','3','4','5','6','7',
+                            '8','9','A','B','C','D','E','F'};
+        while (n > 0) {
+            int rem = n % 16;
+            hex.insert(0, hexDigits[rem]);
+            n = n / 16;
+        }
+        return (hex.length() > 0) ? hex.toString() : "0";
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter an integer: ");
-        int num = sc.nextInt();
+        int number = sc.nextInt();
 
-        String binary = Integer.toBinaryString(num);
-
-        String octal = Integer.toOctalString(num);
-
-        String hex = Integer.toHexString(num);
-
-        System.out.println("Binary: " + binary);
-        System.out.println("Octal: " + octal);
-        System.out.println("Hexadecimal: " + hex.toUpperCase());
-
-        sc.close();
+        System.out.println("Binary: " + toBinary(number));
+        System.out.println("Octal: " + toOctal(number));
+        System.out.println("Hexadecimal: " + toHexadecimal(number));
     }
 }
